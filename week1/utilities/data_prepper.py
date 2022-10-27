07c06a133a1860907288455d5e4353c0525104af
nohanian-proofpoint <nohanian@proofpoint.com>
2022-10-27 23:20:55 +0000

    through L1 Step Extract LTR Logged Features

--- week1/utilities/data_prepper.py
+++ week1/utilities/data_prepper.py
@@ -235,8 +235,7 @@ class DataPrepper:
                                                 self.ltr_store_name,
                                                 size=len(query_doc_ids), terms_field=terms_field)
         ##### Step Extract LTR Logged Features:
-        # IMPLEMENT_START --
-        print("IMPLEMENT ME: __log_ltr_query_features: Extract log features out of the LTR:EXT response and place in a data frame")
+        response = self.opensearch.search(body=log_query, index=self.index_name)
         # Loop over the hits structure returned by running `log_query` and then extract out the features from the response per query_id and doc id.  Also capture and return all query/doc pairs that didn't return features
         # Your structure should look like the data frame below
         feature_results = {}
@@ -245,14 +244,19 @@ class DataPrepper:
         feature_results["sku"] = []
         feature_results["name_match"] = []
         rng = np.random.default_rng(12345)
-        for doc_id in query_doc_ids:
-            feature_results["doc_id"].append(doc_id)  # capture the doc id so we can join later
+        for hit in response['hits']['hits']:
+            doc_id = hit['_id']
+            feature_results["doc_id"].append(doc_id)
             feature_results["query_id"].append(query_id)
-            feature_results["sku"].append(doc_id)  
-            feature_results["name_match"].append(rng.random())
+            feature_results["sku"].append(doc_id) # hm, can we trust this?
+            log_entry = hit['fields']['_ltrlog'][0]['log_entry'][0]
+            if "value" in log_entry:
+                feature_value = log_entry['value']
+            else:
+                feature_value = 0.
+            feature_results["name_match"].append(feature_value)
         frame = pd.DataFrame(feature_results)
         return frame.astype({'doc_id': 'int64', 'query_id': 'int64', 'sku': 'int64'})
-        # IMPLEMENT_END
 
     # Can try out normalizing data, but for XGb, you really don't have to since it is just finding splits
     def normalize_data(self, ranks_features_df, feature_set, normalize_type_map):
